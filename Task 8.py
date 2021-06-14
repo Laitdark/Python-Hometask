5999a4468168068ccb57856fbd2a836754b73b4d
Laitdark <makslaite@gmail.com>
2021-06-14 14:50:56 +0300

    Update Task 8.py
    Поправлен нюанс с годам на 00

--- Task 8.py
+++ Task 8.py
@@ -2,6 +2,12 @@ useryear=int(input("Введите год: "))
 divisionleftover=useryear%4
 
 if divisionleftover==0:
-    print(useryear, "Високосный")
+    if useryear%100==0:
+    	if useryear%400==0:
+    		print(useryear, "Високосный")
+    	else:
+    		print(useryear, "Не високосный")
+    else:
+	    print(useryear, "Високосный")
 else:
-    print(useryear, "Не вискокосный")
+    print(useryear, "Не високосный")
